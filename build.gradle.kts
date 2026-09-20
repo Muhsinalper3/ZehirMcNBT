54724e1be83475749edb8a0b434c1357d8f0ab06
Muhsinalper3 <tapon86750@aganseo.com>
2026-09-20 16:32:35 +0300

    Add ZehirMcNBT Paper plugin

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    java
+}
+
+repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+}
+
+tasks.jar {
+    archiveBaseName.set("ZehirMcNBT")
+}
